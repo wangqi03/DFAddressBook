9a82b3cb29ca71dffee115e4030b29769b4e0fce
wangqi03 <wangqi03@outlook.com>
2017-07-15 18:16:26 +0800

    no message

--- addressBook/addressBook/DFAddressBook/DFAddressBookManager.h
+++ addressBook/addressBook/DFAddressBook/DFAddressBookManager.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AddressBookUI/AddressBookUI.h>
+#import <ContactsUI/ContactsUI.h>
+#import <AddressBook/AddressBook.h>
+#import <Contacts/Contacts.h>
 
+//only one of them will be assigned
+typedef void (^DFAddressBookEnumBlock)(__nullable ABRecordRef person,  CNContact* _Nullable  contact);
+typedef void (^DFAddressBookResultBlock)(BOOL accessGranted);
+
+//the class itself
 @interface DFAddressBookManager : NSObject
+
++ (instancetype)manager;
+
+- (void)requestDeviceAddressBookWithEnumerationBlock:(DFAddressBookEnumBlock)enumeration andResultBlock:(DFAddressBookResultBlock)result;
 
 @end
--- addressBook/addressBook/DFAddressBook/DFAddressBookManager.m
+++ addressBook/addressBook/DFAddressBook/DFAddressBookManager.m
@@ -10,4 +10,57 @@
 
 @implementation DFAddressBookManager
 
+#pragma mark - singleton
++ (instancetype)manager {
+    static DFAddressBookManager *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (void)requestDeviceAddressBookWithEnumerationBlock:(DFAddressBookEnumBlock)enumeration andResultBlock:(DFAddressBookResultBlock)result {
+    if ([[[UIDevice currentDevice] systemVersion] compare:@"9.0" options:NSNumericSearch] == NSOrderedAscending) {
+        ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+        ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+            if (granted) {
+                
+                CFArrayRef ref=ABAddressBookCopyArrayOfAllPeople(addressBook);
+                
+                for (int i=0; i<CFArrayGetCount(ref); i++) {
+                    
+                    ABRecordRef person = CFArrayGetValueAtIndex(ref, i);
+                    
+                    enumeration(person,nil);
+                }
+                
+                CFRelease(ref);
+                
+                result(true);
+            } else {
+                result(false);
+            }
+            
+        });
+    } else {
+        CNContactStore* store=[[CNContactStore alloc] init];
+        [store requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if (granted) {
+                
+                id temp=[CNContactFormatter descriptorForRequiredKeysForStyle:CNContactFormatterStyleFullName];
+                CNContactFetchRequest* request=[[CNContactFetchRequest alloc] initWithKeysToFetch:@[temp,CNContactPhoneNumbersKey]];
+                [store enumerateContactsWithFetchRequest:request error:nil usingBlock:^(CNContact * _Nonnull contact, BOOL * _Nonnull stop) {
+                    
+                    enumeration(nil,contact);
+                }];
+                
+                result(true);
+            } else {
+                result(false);
+            }
+        }];
+    }
+}
+
 @end
